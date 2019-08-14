9ee86cc15916f18387afb1b3e6f8bf1f47f19ec1
Gaurav Pandit <grvkmrpandit@gmail.com>
2019-08-14 11:50:40 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+  def breed
+    @breed
+  end
+ 
+  def breed=(new_name)
+    @breed = new_name
+  end
+  
+ 
+end
